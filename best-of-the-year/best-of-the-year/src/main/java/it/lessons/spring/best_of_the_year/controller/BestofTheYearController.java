6926bfb5f35084b4132ef7165b3de872f8ce7ec4
francescodigiamb <francescodigiamb@gmail.com>
2024-11-12 00:23:54 +0100

    step2 da completare

---
+++ best-of-the-year/best-of-the-year/src/main/java/it/lessons/spring/best_of_the_year/controller/BestofTheYearController.java
@@ -0,0 +1,92 @@
+package it.lessons.spring.best_of_the_year.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import it.lessons.spring.best_of_the_year.model.Movie;
+import it.lessons.spring.best_of_the_year.model.Song;
+
+@Controller
+@RequestMapping ("/")
+public class BestofTheYearController {
+
+	@GetMapping ("/home") 
+	public String home (@RequestParam(name = "name", required = false) String name, Model model) {
+		
+	        // Aggiungi il nome come attributo nel Model
+	        model.addAttribute("name", name);
+
+	        // Restituisci il nome della vista Thymeleaf da renderizzare
+	        return "home";
+	}
+	
+    private List<Movie> getBestMovies() {
+        List<Movie> movies = new ArrayList<>();
+        movies.add(new Movie(1, "Scarface"));
+        movies.add(new Movie(2, "Avengers-End Game"));
+        movies.add(new Movie(3, "Batman-The Dark Knight"));
+        movies.add(new Movie(4, "Inception"));
+        movies.add(new Movie(5, "Interstellar"));
+        
+        return movies;
+    }
+	
+    private List<Song> getBestSongs() {
+    	List<Song> songs = new ArrayList<>();
+    	songs.add(new Song(1, "Crudelia"));
+    	songs.add(new Song(2, "L'alba"));
+    	songs.add(new Song(3, "Il cielo in una stanza"));
+    	songs.add(new Song(4, "Le luci della città"));
+    	
+    	return songs;
+    }
+    
+    @GetMapping("/movies") 
+    public String showMovies(Model model) {
+        List<Movie> movies = getBestMovies();
+        StringBuilder titles = new StringBuilder();
+
+        for (Movie movie : movies) {
+            if (titles.length() > 0) {
+                titles.append(", ");
+            }
+            titles.append(movie.getTitolo());
+        }
+
+        model.addAttribute("titles", titles.toString());
+        
+        return "movies";
+    }
+    
+    @GetMapping("/songs") 
+    public String showSongs(Model model) {
+        List<Song> songs = getBestSongs();
+        StringBuilder titles = new StringBuilder();
+
+        for (Song song : songs) {
+            if (titles.length() > 0) {
+                titles.append(", ");
+            }
+            titles.append(song.getTitolo());
+        }
+
+        model.addAttribute("titles", titles.toString());
+        
+        return "movies";
+    }
+    
+    @GetMapping ("/movies/{id}") 
+    public String idMovies (@RequestParam (name = "titolo", required = false) Model model) {
+    	
+    	
+    	return "idMovies";
+    }
+
+}
---
+++ best-of-the-year/best-of-the-year/src/main/java/it/lessons/spring/best_of_the_year/model/Movie.java
@@ -0,0 +1,28 @@
+package it.lessons.spring.best_of_the_year.model;
+
+public class Movie {
+	private int id;
+	private String titolo;
+	
+	public Movie (int id, String titolo) {
+		this.id = id;
+		this.titolo = titolo;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitolo() {
+		return titolo;
+	}
+
+	public void setTitolo(String titolo) {
+		this.titolo = titolo;
+	}
+	
+}
